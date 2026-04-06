75ab66e9a971d7bee6c157df98a9025334aa5630
silverphantom-only <silverphantom.smcs@gmail.com>
2026-04-06 11:24:15 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,16 @@
-const CACHE_NAME="aguila-v3";
+const CACHE_NAME="aguila-v4";
 
 self.addEventListener("install",e=>{
-e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll([
+e.waitUntil(
+caches.open(CACHE_NAME).then(c=>c.addAll([
 "./","./index.html","./app.js","./manifest.json"
-])));
+]))
+);
 self.skipWaiting();
 });
 
 self.addEventListener("fetch",e=>{
-e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+e.respondWith(
+caches.match(e.request).then(r=>r||fetch(e.request))
+);
 });
